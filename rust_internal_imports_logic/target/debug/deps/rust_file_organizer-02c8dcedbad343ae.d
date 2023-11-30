60859629e9b5d1c036239c8b61d528115b481ecb
JoaoHFerreira <jaycops@gmail.com>
2023-11-30 02:52:44 -0300

    example on how to use imports in rust

---
+++ rust_internal_imports_logic/target/debug/deps/rust_file_organizer-02c8dcedbad343ae.d
@@ -0,0 +1,8 @@
+/home/joao_ferreira/sandbox/FileOrganizer/rust_file_organizer/target/debug/deps/rust_file_organizer-02c8dcedbad343ae.rmeta: src/main.rs src/other_file_same_dir.rs src/some_pack/mod.rs src/some_pack/file_from_other_package.rs
+
+/home/joao_ferreira/sandbox/FileOrganizer/rust_file_organizer/target/debug/deps/rust_file_organizer-02c8dcedbad343ae.d: src/main.rs src/other_file_same_dir.rs src/some_pack/mod.rs src/some_pack/file_from_other_package.rs
+
+src/main.rs:
+src/other_file_same_dir.rs:
+src/some_pack/mod.rs:
+src/some_pack/file_from_other_package.rs:
---
+++ rust_internal_imports_logic/target/debug/deps/rust_file_organizer-695adf3a2ae43c11.d
@@ -0,0 +1,8 @@
+/home/joao_ferreira/sandbox/FileOrganizer/rust_file_organizer/target/debug/deps/rust_file_organizer-695adf3a2ae43c11: src/main.rs src/other_file_same_dir.rs src/some_pack/mod.rs src/some_pack/file_from_other_package.rs
+
+/home/joao_ferreira/sandbox/FileOrganizer/rust_file_organizer/target/debug/deps/rust_file_organizer-695adf3a2ae43c11.d: src/main.rs src/other_file_same_dir.rs src/some_pack/mod.rs src/some_pack/file_from_other_package.rs
+
+src/main.rs:
+src/other_file_same_dir.rs:
+src/some_pack/mod.rs:
+src/some_pack/file_from_other_package.rs:
